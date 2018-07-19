62bf3b96eab92ab332a9cbda686daeeed2f9f302
Noskcaj <kcajdev@gmail.com>
2018-07-18 20:05:15 -0400

    Add pixel transport support to kitty escapes

--- src/renderer/kitty.rs
+++ src/renderer/kitty.rs
@@ -1,7 +1,6 @@
 use base64;
-use image::Frames;
-use std::fs;
-use std::io::{self, stdin, stdout, BufReader, Read, Write};
+use image::{self, DynamicImage, FilterType, Frames, GenericImage};
+use std::io::{self, stdin, stdout, Read, Write};
 use termion::raw::IntoRawMode;
 use Options;
 
@@ -19,34 +18,65 @@ fn print_cmd_payload(cmds: &[(&str, &str)], payload: &str) -> io::Result<()> {
     let stdout = stdout();
     let mut stdout = stdout.lock();
 
-    stdout.write(PROTOCOL_START)?;
-    let mut buf_reader = BufReader::new(payload.as_bytes());
-    let mut buf = [0; MAX_BUFFER];
-    let mut bytes_read = buf_reader.read(&mut buf)?;
-    while bytes_read > MAX_BUFFER {
+    let mut payload = payload.as_bytes();
+    while payload.len() > MAX_BUFFER {
+        let (buf, new_payload) = payload.split_at(MAX_BUFFER);
+        payload = new_payload;
         stdout.write(PROTOCOL_START)?;
         write!(stdout, "{},m=1;", cmds)?;
         stdout.write(&buf)?;
-        bytes_read = buf_reader.read(&mut buf)?;
+        stdout.write(PROTOCOL_END)?;
     }
+    stdout.write(PROTOCOL_START)?;
     write!(stdout, "{},m=0;", cmds)?;
-    stdout.write(&buf[..bytes_read])?;
+    stdout.write(&payload)?;
     stdout.write(PROTOCOL_END)?;
+
     stdout.flush()?;
     Ok(())
 }
 
+/// Display an image from an canonical path
+///
+/// # Notes
+/// Path _must_ be canonical, use std::fs::canonicalize
+#[allow(dead_code)]
 fn display_path(path: &str) -> io::Result<()> {
     let payload = base64::encode_config(path.as_bytes(), base64::STANDARD);
 
     print_cmd_payload(
-        &[("f", "100"), ("a", "T"), ("i", "1"), ("t", "f")],
+        &[("f", "100"), ("a", "T"), ("i", "0"), ("t", "f")],
         &payload,
     )?;
 
     Ok(())
 }
 
+fn display_image(img: DynamicImage, _max_size: (u16, u16)) -> io::Result<()> {
+    let (width, height) = img.dimensions();
+    let (data, bits) = match img {
+        DynamicImage::ImageRgb8(rgb) => (rgb.into_vec(), 24),
+        DynamicImage::ImageRgba8(rgba) => (rgba.into_vec(), 32),
+        _ => unimplemented!("IMG Variant"),
+    };
+    let payload = base64::encode_config(&data, base64::STANDARD);
+
+    print_cmd_payload(
+        &[
+            ("f", &bits.to_string()),
+            ("a", "T"),
+            ("i", "0"),
+            ("t", "d"),
+            ("s", &width.to_string()),
+            ("v", &height.to_string()),
+        ],
+        &payload,
+    )?;
+    Ok(())
+}
+
+// TODO: Find out why terminal isnt sending anything
+#[allow(dead_code)]
 fn read_term_response() -> io::Result<()> {
     let mut stdout = stdout().into_raw_mode()?;
     let mut stdin = stdin();
@@ -67,10 +97,11 @@ fn read_term_response() -> io::Result<()> {
     Ok(())
 }
 
-pub fn display(_options: &Options, _max_size: (u16, u16), path: &str) -> io::Result<()> {
-    display_path(&fs::canonicalize(path)?.to_string_lossy())?;
+pub fn display(_options: &Options, max_size: (u16, u16), path: &str) -> io::Result<()> {
+    let img = image::open(path).unwrap();
+    display_image(img, max_size)?;
 
-    read_term_response()?;
+    // read_term_response()?;
 
     Ok(())
 }
